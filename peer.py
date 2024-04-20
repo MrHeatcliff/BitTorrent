53dc02dfbd27ce30751804074397e66a78167aeb
heathcliff <dat.tranheathcliff@hcmut.edu.vn>
2024-04-20 18:22:11 +0700

    split file, send 1 peer to other peer

--- peer.py
+++ peer.py
@@ -1,36 +1,30 @@
-# Client
+import tqdm
+import os, threading, platform
 import socket
-import pickle
-import socket
-from threading import Thread
+import hashlib
 from pathlib import Path
 
-OUTPUT_FILE = Path("./new/newfile.webp")
-myList = []
+IP = '192.168.1.33'
+PORT = 22236
+ADDR = (IP, PORT)
+
 PEER_SERVER_IP = '192.168.1.33'
-PEER_SERVER_PORT = 11234
+PEER_SERVER_PORT = 11235
 
-def new_connection(addr, conn):
-    data = conn.recv(1024).decode()
-    print(data)
-    conn.send("hello".encode())
+FORMAT = "utf-8"
+SIZE = 1024
+PEER_PATH = "./client_data"
+PEER1_PATH = "./peer_data"
+SERVER_DATA_PATH = "./server_data" #need define
 
-def peer_server(host, port):
-    peerServerSocket = socket.socket()
-    peerServerSocket.bind((host, port))
+if not os.path.exists(PEER_PATH):
+    os.makedirs(PEER_PATH)
 
-    peerServerSocket.listen(10)
-    while True:
-        conn, addr = peerServerSocket.accept()
-        nconn = Thread(target = new_connection, args = (addr, conn))
-        nconn.start()
+if not os.path.exists(SERVER_DATA_PATH):
+    os.makedirs(SERVER_DATA_PATH)
+PIECE_SIZE = 1024
 
-def peer_client(host, port):
-    client_socket = socket.socket()
-    client_socket.connect((host, port))
-    message = "HELLO"
-    client_socket.send(message.encode())
-    client_socket.recv(1024).decode()
+client_list = []
 
 def separate_string(data):
     data = data.replace("[", "")
@@ -42,53 +36,229 @@ def separate_string(data):
         splitData[i] = int(splitData[i])
     return splitData
 
+def handlePconnect(conn, addr):
+    print(f"Sender: {addr}")
+    data = conn.recv(1024).decode()
+    print(data)
+    conn.send("Chuan bi nhan ne".encode())
+
+    filename = conn.recv(1024).decode()
+    filepath = os.path.join(SERVER_DATA_PATH, filename)
+    pieces, piece_hashes = split_file_into_pieces(Path(filepath), PIECE_SIZE)
+    for piece, piece_hash in zip(pieces, piece_hashes):
+        data = piece
+        conn.send(data)
+    conn.send(b'#END')
+
+
+def peer_client(host, port, filename):
+    p_client = socket.socket()
+    p_client.connect((host, port))
+
+    mes = "Ok bro"
+    p_client.send(mes.encode(FORMAT))
+    print(p_client.recv(1024).decode())
+
+    print(f"Receiver: {p_client.getsockname()}")
+
+    p_client.send(filename.encode(FORMAT))
+
+    filepath = os.path.join(PEER_PATH, filename)
+
+    # start = p_client.recv(SIZE).decode(FORMAT)
+    # mes, size = start.split('@')
+    # p_client.send(mes.encode(FORMAT))
+    
+    # if mes == "Start downloading..":
+    pieces = []
+    piece_hashes =[]
+    end = False
+    while (not end):
+        piece = p_client.recv(1024)
+        if (b'#END' in piece):
+            piece = piece[:-4]
+            pieces.append(piece)
+            end = True
+            break
+        pieces.append(piece)
+    join_pieces(pieces, filepath)
+    p_client.send("Finished Downloading.".encode(FORMAT))  
+
+    
+    
+
+def peer_server(p_server):
+    p_server.listen(10)
+
+    while True:
+        try:
+            print("wait....")
+            conn, addr = p_server.accept()
+            print("connected")
+            thrHandle = threading.Thread(target=handlePconnect, args=(conn, addr))
+            thrHandle.start()
+        except:
+            print("Your connection is not successful.")
+            break
+
+def recv(client):
+    while True:
+        print("startloop")
+        data = client.recv(SIZE).decode(FORMAT)
+        cmd, msg = data.split("@")
+
+        if cmd == "DISCONNECTED":
+            print(f"[SERVER]: {msg}")
+            break
+        elif cmd == "OK":
+            print(f"{msg}")
+        elif cmd == "FIND":
+            print(f"{msg}")
+            filepath = os.path.join(SERVER_DATA_PATH, msg)
+
+            if(os.path.exists(filepath)):
+                client.send("YES".encode())
+
+        elif cmd == "GIVE":
+            print(f"{msg}")
+            otherPeer = msg.split(",")
+
+            peerName = otherPeer[0]
+            peerName = peerName.replace("'", "")
+            peerPort = int(otherPeer[1])
+            
+            print(peerName)
+            print(peerPort)
+            thrClient = threading.Thread(target=peer_client, args=(peerName, peerPort))
+            thrClient.start()
+
+def sen(client):
+    while True:
+        print("startloop")
+        data = client.recv(SIZE).decode(FORMAT)
+        print(data)
+        data = data.split("@")
+        cmd, msg = data[0], data[1]
+
+        if cmd == "DISCONNECTED":
+            print(f"[SERVER]: {msg}")
+            break
+        elif cmd == "OK":
+            print(f"{msg}")
+        elif cmd == "FIND":
+            print(f"{msg}")
+            filepath = os.path.join(SERVER_DATA_PATH, msg)
+
+            if(os.path.exists(filepath)):
+                client.send("YES".encode())
+        elif cmd == "GIVE":
+            print(f"{msg}")
+            otherPeer = msg.split(",")
+
+            peerName = otherPeer[0]
+            peerName = peerName.replace("'", "")
+            peerPort = int(otherPeer[1])
+            
+            print(peerName)
+            print(peerPort)
+            thrClient = threading.Thread(target=peer_client, args=(peerName, peerPort, data[2]))
+            thrClient.start()
+        data = input("> ")
+        data = data.split(" ")
+        cmd = data[0]
+
+        if cmd == "HELP":
+            client.send(cmd.encode(FORMAT))
+        elif cmd == "LOGOUT":
+            client.send(cmd.encode(FORMAT))
+            break
+        elif cmd == "PEERS":
+            client.send(cmd.encode(FORMAT))
+        elif cmd == "REQ":
+            client.send(f"{cmd}@{data[1]}".encode(FORMAT))
+        elif cmd == "LIST":
+            client.send(cmd.encode(FORMAT))
+        elif cmd == "DELETE":
+            client.send(f"{cmd}@{data[1]}".encode(FORMAT))
+        elif cmd == "UPLOAD":
+            filename = data[1]
+            send_data = f"{cmd}@{filename}"
+            client.send(send_data.encode(FORMAT))
+            
+        elif cmd == "DOWNLOAD":
+            filename = data[1]
+
+            peerIp = data[2]
+            peerport = data[3]
+            client.send(f"{cmd}@{filename}:{peerIp} {peerport}".encode(FORMAT))
+        else:
+            print("pass")
+            client.send("pass".encode())
+
+        print("endloop")
+
+    print("Disconnected from the server.")
+    client.close()
+
+def main():
+    client_to_tracker = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_to_tracker.connect(ADDR)
+    message = "NOPE"
+    p_server = socket.socket()
+    p_server.bind((PEER_SERVER_IP, PEER_SERVER_PORT))
+    print(p_server.getsockname())
+
+    client_to_tracker.send(str(p_server.getsockname()).encode(FORMAT))
+    ser = threading.Thread(target = peer_server, args = (p_server,))
+    ser.start()
+
+    sen(client_to_tracker)
+    try:
+        p_server.close()
+    except:
+        ser.join()
+        print("Disconnected from the server.")        
+
+def split_file_into_pieces(file_path, piece_size):
+    file_path = Path(file_path)
+    if(file_path.is_file()):
+        # get file size
+        file_size = os.path.getsize(file_path)
+
+        # count number of pieces
+        num_pieces = file_size // piece_size
+        if file_size % piece_size != 0:
+            num_pieces += 1
+
+        pieces = []
+        piece_hashes = []
+
+        with open(file_path, 'rb') as f:
+            for i in range(num_pieces):
+                # read data from file
+                piece_data = f.read(piece_size)
+                pieces.append(piece_data)
+
+                # hashing
+                piece_hash = hashlib.sha1(piece_data).digest()
+                piece_hashes.append(piece_hash)
+
+        return pieces, piece_hashes
+
 def join_pieces(pieces, output_file_path):
     # create file
     with open(output_file_path, 'wb') as new_file:
         for piece in pieces:
             # hash checking
             # if hashlib.sha1(piece).digest() != piece_hash:
-                # raise ValueError("Hash của piece không khớp với hash trong torrent file")
+            #     raise ValueError("Hash của piece không khớp với hash trong torrent file")
 
             # write data
-            new_file.write(piece) 
+            new_file.write(piece)    
 
 if __name__ == "__main__":
-    host = "192.168.1.33"
-    port = 22236
-    client_socket = socket.socket()
-    client_socket.connect((host, port))
-    message = "NOPE"
-    ser = Thread(target = peer_server, args = ((PEER_SERVER_IP, PEER_SERVER_PORT)))
-    ser.start()
-    while message.lower().strip() != "bye":
-        match message:
-            case "GET LIST":
-                client_socket.send(message.encode())
-                data = client_socket.recv(1024).decode()
-                myList = separate_string(data)
-                print(myList)
-            case "DOWNLOAD":
-                client_socket.send(message.encode())
-                pieces = []
-                piece_hashes =[]
-                print("Downloading:", end="\n")
-                end = False
-                while (not end):
-                    # piece = pickle.load(client_socket.recv(1024))
-                    piece = client_socket.recv(1024)
-                    if (b'#END' in piece):
-                        # piece = piece.strip(b'#END')
-                        end = True
-                        break
-                    pieces.append(piece)
-                    # print('Piece:', piece)
-                    # print('hash:', piece_hash)
-                    # client_socket.send(b'ACK')
-                join_pieces(pieces,OUTPUT_FILE)
-                print("download complete")
-            case _:
-                client_socket.send(message.encode())
-                data = client_socket.recv(1024).decode()
-        message = input(" -> ")  # again take input
-    client_socket.close()
+    # filename = 'Brabbit.webp'
+    # filepath = os.path.join(Path(SERVER_DATA_PATH), filename)
+    # pieces, piece_hashes = split_file_into_pieces(filepath, PIECE_SIZE)
+    # print(pieces)
+    main()
--- server.py
+++ server.py
@@ -1,76 +1,163 @@
-# Server
+# from makeTorrent import makeTorrent
+import tqdm
+import os, threading, platform
 import socket
-import pickle
 import hashlib
 from pathlib import Path
-import os
-from threading import Thread
 
-file_path = Path('./Brabbit.webp')
-file_exists = file_path.is_file()
-fileList = ["Brabbit.webp"]
-PIECE_SIZE = 1024
+IP = "192.168.1.33" # 
+PORT = 22236
+ADDR = (IP, PORT)
+SIZE = 1024
+FORMAT = "utf-8"
+SERVER_DATA_PATH = "server_data" #need define
 
-def split_file_into_pieces(file_path, piece_size):
-    if(file_exists):
-        # get file size
-        file_size = os.path.getsize(file_path)
+client_list = []
+client_ips = []
+peer_has_file = []
+file_list = {}
 
-        # count number of pieces
-        num_pieces = file_size // piece_size
-        if file_size % piece_size != 0:
-            num_pieces += 1
-
-        pieces = []
-        piece_hashes = []
-
-        with open(file_path, 'rb') as f:
-            for i in range(num_pieces):
-                # read data from file
-                piece_data = f.read(piece_size)
-                pieces.append(piece_data)
-
-                # hashing
-                piece_hash = hashlib.sha1(piece_data).digest()
-                piece_hashes.append(piece_hash)
-
-        return pieces, piece_hashes
-
-def new_connection(addr, conn):
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+    conn.send("OK@Welcome to the File Server.\nPress 'HELP' to show all command.".encode(FORMAT))
     while True:
-        data = conn.recv(1024).decode()
-        print("Receive from client: " + str(data))
-        match data:
-            case "GET LIST":
-                conn.send(str(fileList).encode())
-            case "DOWNLOAD":
-                pieces, piece_hashes = split_file_into_pieces(file_path, PIECE_SIZE)
-                # conn.send(pieces[0])
-                for piece, piece_hash in zip(pieces, piece_hashes):
-                    # data = pickle.dump((piece, piece_hash))
-                    data = piece
-                    conn.send(data)
-                    # ack = conn.recv(1024)
-                    # if ack != b'ACK':
-                #         print("ERROR")
-                #         break
-                conn.send(b'#END')
+        data = conn.recv(SIZE).decode(FORMAT)
+        data = data.split("@")
+        cmd = data[0]
 
-            case _:
-                conn.send("Received".encode())
-        print(fileList)
+        if cmd == "LIST":
+            send_data = "OK@"
+            if len(file_list) == 0:
+                send_data += "There is no file for you to access."
+            else:
+                send_data += str(file_list)
+            conn.send(send_data.encode(FORMAT))
+        elif cmd == "YES":
+            peer_has_file.append(conn)
+        elif cmd == "PEERS":
+            send_data = "OK@"
+            send_data += str(client_ips)
+            conn.send(send_data.encode(FORMAT))
+        elif cmd == "REQ":
+            print(len(client_list))
+            for client in client_list:
+                print(client)
+                client.send(f"FIND@{data[1]}".encode(FORMAT))
+                sock = client.recv(SIZE).decode(FORMAT)
+                print(sock)
+                if(sock == "YES"):
+                    peer_has_file.append(client)
+            print(peer_has_file)
+            print(client_list)
+            conn.send(f"GIVE@{client_ips[client_list.index(peer_has_file[0])]}".encode(FORMAT))
+            
+        elif cmd == "UPLOAD":
+            filename = data[1]
 
-def server_program(host, port):
-    serversocket = socket.socket()
-    serversocket.bind((host, port))
+            temp_list = file_list[client_ips[client_list.index(conn)]]
+            temp_list.append(filename)
 
-    serversocket.listen(10)
-    while True: 
-        conn, addr = serversocket.accept()
-        nconn = Thread(target=new_connection, args=(addr, conn))
-        nconn.start()
+            file_list[client_ips[client_list.index(conn)]] = temp_list
+
+            conn.send("OK@Server has received your file.".encode())
+
+            print(file_list)
+
+        elif cmd == "DOWNLOAD":
+            rec_data = data[1]
+            print(rec_data)
+            rec_data = rec_data.split(":")
+            print(rec_data)
+            filename = rec_data[0]
+
+            peername = rec_data[1]
+            peername = peername.split(",")
+
+            peerIp = peername[0]
+            peerPort = peername[1]
+            sender_server_id = client_ips.index(f"'{peerIp}',{peerPort}")
+            sender = client_list[sender_server_id]
+
+            sock = -1
+            try:
+                print(file_list[client_ips[sender_server_id]])
+                sock = file_list[client_ips[sender_server_id]].index(filename)
+            except:
+                sock = -1
+            if(sock < 0):
+                conn.send(f"OK@There is no '{filename}' in {peername}".encode(FORMAT))
+            else :
+                conn.send(f"GIVE@{client_ips[client_list.index(sender)]}@{filename}".encode(FORMAT))
+
+        elif cmd == "DELETE":
+            files = os.listdir(SERVER_DATA_PATH)
+            send_data = "OK@"
+            filename = data[1]
+
+            if len(files) == 0:
+                send_data += "The server directory is empty"
+            else:
+                if filename in files:
+                    if(platform.system() == "Linux"):
+                        filepath = f"rm ./"
+                    else:
+                        filepath = f"del .\\"
+
+                    filepath += os.path.join(SERVER_DATA_PATH, filename)
+
+                    print(filepath)
+                    os.system(filepath)
+                    send_data += "File deleted successfully."
+                else:
+                    send_data += "File not found."
+
+            conn.send(send_data.encode(FORMAT))
+
+        elif cmd == "LOGOUT":
+            break
+        elif cmd == "HELP":
+            data = "OK@"
+            data += "LIST: List all peers and its file.\n"
+            data += "PEERS: List all peers.\n"
+            data += "REQ <file>: Find <file> and download it from 1 peer has it \n"
+            data += "UPLOAD <filename>: Upload file to the server know you have it.\n"
+            # data += "DOWNLOAD <path>: Download a file from  the server.\n"
+            data += "DOWNLOAD <file> <peername>: Download <file> from <peername>.\n"
+            # data += "DELETE <filename>: Delete a file from the server.\n"
+            data += "LOGOUT: Disconnect from the server.\n"
+            data += "HELP: List all the commands."
+
+            conn.send(data.encode(FORMAT))
+        else:
+            conn.send("OK@metvl".encode())
+
+    print(f"[DISCONNECTED] {addr} disconnected")
+    client_list.remove(conn)
+    conn.close()
+
+def main():
+    print("[STARTING] Server is starting")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    server.listen()
+    print(f"[LISTENING] Server is listening on {IP}:{PORT}.")
+
+    while True:
+        conn, addr = server.accept()
+
+        peerSocket = conn.recv(1024).decode(FORMAT)
+        peerSocket = peerSocket.replace("(", "")
+        peerSocket = peerSocket.replace(")", "")
+
+        file_list[peerSocket] = []
+        client_ips.append(peerSocket)
+        client_list.append(conn)
+        try:
+            thread = threading.Thread(target=handle_client, args=(conn, addr))
+            thread.start()
+        except:
+            print("error")
+            break
 
 if __name__ == "__main__":
-    host = "192.168.1.33"
-    port = 22236
-    server_program(host, port)
+    main()
